74dbdcd8bff7e04adc1f82e55d2d6d475a26ccc7
Robson Sousa <rcls7723@gmail.com>
2021-09-07 14:34:23 -0300

    Inclusão BarChart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
@@ -7,7 +8,17 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá Mundo!</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5>Todas Vendas</h5>
+            <BarChart />
+
+          </div>
+
+        </div>
+
         <DataTable />
       </div>
       <Footer />
